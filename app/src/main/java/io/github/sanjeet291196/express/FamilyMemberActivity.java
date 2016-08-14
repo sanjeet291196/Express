dd8db227e4ef9e08704ee28558f411b94586ab46
sanjit <sanjeet291196@gmail.com>
2016-08-14 18:02:07 +0530

    Completed Family Member Activity

--- app/src/main/java/io/github/sanjeet291196/express/FamilyMemberActivity.java
+++ app/src/main/java/io/github/sanjeet291196/express/FamilyMemberActivity.java
@@ -2,6 +2,9 @@ package io.github.sanjeet291196.express;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyMemberActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class FamilyMemberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family_member);
+
+        ArrayList<DataItem> dataItems = new ArrayList<>();
+        dataItems.add(new DataItem(R.drawable.family_grandfather, "Grand Father", "Daada(दादा)", "Tāta"));
+        dataItems.add(new DataItem(R.drawable.family_grandmother, "Grand Mother", "Daadee ma(दादी मा)", "Am'mam'ma"));
+        dataItems.add(new DataItem(R.drawable.family_father, "Father", "Pita(पिता)", "Taṇḍri"));
+        dataItems.add(new DataItem(R.drawable.family_mother, "Mother", "Maan(मां)", "Talli"));
+        dataItems.add(new DataItem(R.drawable.family_son, "Son", "Beta(बेटा)", "Kumāruḍu"));
+        dataItems.add(new DataItem(R.drawable.family_daughter, "Daughter", "Betee(बेटी)", "kumārte"));
+        dataItems.add(new DataItem(R.drawable.family_older_brother, "Elder Brother", "Bada Bhaee(बड़ा भाई)", "Annayya"));
+        dataItems.add(new DataItem(R.drawable.family_older_sister, "Elder Sister", "Badee Bahan(बड़ी बहन)", "Akka"));
+        dataItems.add(new DataItem(R.drawable.family_younger_brother, "Younger Brother", "Chhota Bhaee(छोटा भाई)", "Tam'muḍu"));
+        dataItems.add(new DataItem(R.drawable.family_younger_sister, "Younger Sister", "Chhotee Bahan(छोटी बहन)", "Chinna Celli"));
+
+        DataAdapter adapter = new DataAdapter(this, dataItems);
+        ListView familyItemListView = (ListView) findViewById(R.id.FamilyItemListView);
+        familyItemListView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
     }
 }
