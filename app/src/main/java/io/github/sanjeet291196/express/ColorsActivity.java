d2bcc2ca9895b7411c07b2257c540101c6e87974
sanjit <sanjeet291196@gmail.com>
2016-08-14 14:27:39 +0530

    Created Menu

---
+++ app/src/main/java/io/github/sanjeet291196/express/ColorsActivity.java
@@ -0,0 +1,13 @@
+package io.github.sanjeet291196.express;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ColorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colors);
+    }
+}
